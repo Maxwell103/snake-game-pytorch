4958fd9ef48a80c80f65c2111d8f858950a81981
Maxwell103 <m.ernst@student.fontys.nl>
2023-04-14 12:36:52 +0200

    j

--- agent.py
+++ agent.py
@@ -3,9 +3,11 @@ import random
 import numpy as np
 from collections import deque
 from game import SnakeGameAI, Direction, Point
+from model import Linear_QNet, QTrainer
+from helper import plot
 
 #Can play around with these
-MAX_MEMORY = = 100_000
+MAX_MEMORY = 100_000
 BATCH_SIZE = 1000
 LR = 0.001
 
@@ -14,10 +16,10 @@ class Agent:
         def __init__(self):
             self.n_games = 0
             self.epsilon = 0 #control the randomness
-            self.gamma = 0 #discount rate
+            self.gamma = 0.9 #discount rate
             self.memory = deque(maxlen = MAX_MEMORY) # calls popleft()
-            self.model = None # TODO
-            self.trainer = None # TODO
+            self.model = Linear_QNet(11,256,3)  #input, hidden, output
+            self.trainer = QTrainer(self.model, lr= LR, gamma = self.gamma)
             # TODO: model, trainer
         
         def get_state(self, game):
@@ -94,7 +96,7 @@ class Agent:
                 final_move[move] = 1
             else:
                 state0 = torch.tensor(state, dtype = torch.float)
-                prediction = self.model.predict(state0)
+                prediction = self.model(state0)
                 move = torch.argmax(prediction).item()
                 final_move[move] = 1
                 
@@ -132,11 +134,16 @@ def train():
             
             if score > record :
                 record = score
-                # agent.model.save()
+                agent.model.save()
                 
             print('Game:', agent.n_games,'Score:', score, 'Record:', record)
             
-            # TODO Plot
-
-if __name__ == '__main__':
+            plot_scores.append(score)
+            total_score += score
+            mean_score = total_score / agent.n_games
+            plot_means_scores.append(mean_score)
+            plot(plot_scores, plot_mean_scores)
+            
+            
+if __name__ == "__main__":
     train()
